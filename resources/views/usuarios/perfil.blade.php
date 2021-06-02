dd087f333a4316f5bd00f447906eb5f8b557d482
Miguel Castilla May <50216221+MiguelCastillaMay@users.noreply.github.com>
2021-06-02 14:48:18 -0500

    Se agregó la barra de búsqueda

--- resources/views/usuarios/perfil.blade.php
+++ resources/views/usuarios/perfil.blade.php
@@ -6,11 +6,12 @@
     <div class="menuBar">
         <h1>TiendaFicticia.com</h1>
         <ul>
-            <li><a href="#">Menú</a></li>
             <li><a href="#">Categorías</a></li>
-            <li><a href="#">Ofertas</a></li>
-            <li><a href="#">Mi perfil</a></li>
-            <li><a href="#">Mi carrito</a></li>
+            <li><a href="#">Productos</a></li>
+            <li><form action="/search" method="get" role="search">
+                <input type="text" name="find" placeholder="Buscar productos">
+                <button type="submit">Buscar</button>
+            </form></li>
         </ul>
     </div>
 @endsection
@@ -22,8 +23,8 @@
             <p id="nombre">{{ $usuario->nombre }} {{ $usuario->a_paterno }} {{ $usuario->a_materno }}</p>
             <p>{{ $usuario->correo }}</p>
             <p>{{ $usuario->rol }}</p>
-            <button id="botonInverso"><a href="/usuario/edit/{{ $usuario->id }}">Editar perfil</a></button>
+            <button id="botonInverso"><a href="/usuario/edit/{{ $usuario->usuarioID }}">Editar perfil</a></button>
         </div>
     </div>
-    <a href="/login"><button id="botonInverso" class="pafuera">Salir pa fuera</button></a>
+    <a href="/salir"><button id="botonInverso" class="pafuera">Salir pa fuera</button></a>
 @endsection
