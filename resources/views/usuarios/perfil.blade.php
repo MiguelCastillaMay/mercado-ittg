b55e5b7ba9982accc840b1ad712caacf4db43e84
Miguel Castilla May <50216221+MiguelCastillaMay@users.noreply.github.com>
2021-06-05 14:44:11 -0500

    Se agregaron las rutas de la barra de navegación

--- resources/views/usuarios/perfil.blade.php
+++ resources/views/usuarios/perfil.blade.php
@@ -6,8 +6,8 @@
     <div class="menuBar">
         <h1>TiendaFicticia.com</h1>
         <ul>
-            <li><a href="#">Categorías</a></li>
-            <li><a href="#">Productos</a></li>
+            <li><a href="/categoria">Categorías</a></li>
+            <li><a href="/productos">Productos</a></li>
             <li><form action="/search" method="get" role="search">
                 <input type="text" name="find" placeholder="Buscar productos">
                 <button type="submit">Buscar</button>
