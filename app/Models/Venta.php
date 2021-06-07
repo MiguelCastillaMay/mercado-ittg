21ce0ff4aea63ad3bd8ab383c9908211f55c7113
Miguel Castilla May <50216221+MiguelCastillaMay@users.noreply.github.com>
2021-06-06 19:59:20 -0500

    Se agregaron las variables necesarias en el modelo

--- app/Models/Venta.php
+++ app/Models/Venta.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Venta extends Model
 {
+    public $timestamps = false;
+    public $table = 'ventas';
+    protected $primaryKey = 'ventaID';
     use HasFactory;
+
 }
