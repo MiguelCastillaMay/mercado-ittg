f67db4545f30e16f8435d2f74539d4db6a081c5a
Miguel Castilla May <50216221+MiguelCastillaMay@users.noreply.github.com>
2021-06-06 19:32:38 -0500

    Variable $timestamps del modelo Usuario se camibó a true

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Usuario extends Authenticatable {
-    public $timestamps = false;
+    public $timestamps = true;
     public $table = 'usuarios';
     protected $primaryKey = 'usuarioID';
     protected $fillable = ['nombre','a_paterno','a_materno','correo','imagen','rol','activo','password'];
