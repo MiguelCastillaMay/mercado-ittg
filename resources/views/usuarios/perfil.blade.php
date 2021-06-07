f193e8718852dcafd623442cc9751311d8f08a47
Miguel Castilla May <50216221+MiguelCastillaMay@users.noreply.github.com>
2021-06-06 20:00:49 -0500

    Esta vista ahora trabaja en función del usuario loggeado

--- resources/views/usuarios/perfil.blade.php
+++ resources/views/usuarios/perfil.blade.php
@@ -2,29 +2,54 @@
 
 @section('title', 'Mi perfil')
 
+@php
+    $usuarioAuth = Auth::User()
+@endphp
+
 @section('navBar')
     <div class="menuBar">
         <h1>TiendaFicticia.com</h1>
         <ul>
-            <li><a href="/categoria">Categorías</a></li>
+            <li><a href="/supervisor">Menú</a></li>
+            <li><a href="/usuarios">Usuarios</a></li>
             <li><a href="/productos">Productos</a></li>
-            <li><form action="/search" method="get" role="search">
-                <input type="text" name="find" placeholder="Buscar productos">
-                <button type="submit">Buscar</button>
-            </form></li>
+            @if ($usuarioAuth->rol == 'Cliente')
+                <li><form action="/search" method="get" role="search">
+                    <input type="text" name="find" placeholder="Buscar productos">
+                    <button type="submit">Buscar</button>
+                </form></li>
+            @elseif ($usuarioAuth->rol == 'Supervisor' or $usuarioAuth->rol == 'Revisor')
+                <li><a href="/categoria">Categorías</a></li>
+                <li><a href="/bitacora">Bitácora</a></li>
+            @endif
         </ul>
     </div>
 @endsection
 
 @section('contenido')
+    @if ($usuarioAuth->rol == 'Cliente')
+        <div id="datosPersonales">
+            <img src="{{ url('storage/'.$usuario->imagen) }}">
+            <div>
+                <p id="nombre">{{ $usuario->nombre }} {{ $usuario->a_paterno }} {{ $usuario->a_materno }}</p>
+                <p>{{ $usuario->correo }}</p>
+                <p>{{ $usuario->rol }}</p>
+                <button id="botonInverso"><a href="/usuario/edit/{{ $usuario->usuarioID }}">Editar perfil</a></button>
+            </div>
+        </div>
+    @elseif ($usuarioAuth->rol == 'Supervisor')
     <div id="datosPersonales">
         <img src="{{ url('storage/'.$usuario->imagen) }}">
         <div>
             <p id="nombre">{{ $usuario->nombre }} {{ $usuario->a_paterno }} {{ $usuario->a_materno }}</p>
-            <p>{{ $usuario->correo }}</p>
             <p>{{ $usuario->rol }}</p>
+            <p>{{ $usuario->correo }}</p>
+            <p>Fecha de registro: {{ $usuario->created_at }}</p>
+            <p>Ventas totales: {{ $ventas }}</p>
+            <p>Productos en venta: {{ $productos }}</p>
             <button id="botonInverso"><a href="/usuario/edit/{{ $usuario->usuarioID }}">Editar perfil</a></button>
         </div>
     </div>
+    @endif
     <a href="/salir"><button id="botonInverso" class="pafuera">Salir pa fuera</button></a>
 @endsection
