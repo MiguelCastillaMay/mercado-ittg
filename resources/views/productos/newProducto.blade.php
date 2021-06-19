c99fe422dc44bcc82d6a6b07e57d4bcc960b9d69
Miguel Castilla May <50216221+MiguelCastillaMay@users.noreply.github.com>
2021-06-19 16:07:14 -0500

    Cambios en el diseño

--- resources/views/productos/newProducto.blade.php
+++ resources/views/productos/newProducto.blade.php
@@ -1,6 +1,33 @@
 @extends('layout')
 
 @section('title', 'Nuevo producto')
+
+@php
+    $usuarioAuth = Auth::User();
+@endphp
+
+@section('navBar')
+    <div class="menuBar">
+        <h1>Mercado ITTG</h1>
+        <ul>
+            <li><a href="/categoria">Categorías</a></li>
+            <li><a href="/productos">Productos</a></li>
+            @if (is_null($usuarioAuth) or $usuarioAuth->rol == 'Cliente')
+                <li><form action="/search" method="get" role="search">
+                    <input type="text" name="find" placeholder="Buscar productos">
+                    <button type="submit">Buscar</button>
+                </form></li>
+            @elseif ($usuarioAuth->rol == 'Supervisor' or $usuarioAuth->rol == 'Revisor')
+                <li><a href="/productos">Bitácora</a></li>
+            @endif
+            @if (is_null($usuarioAuth))
+                <li><a href="/login">Iniciar sesión</a></li>
+            @elseif ($usuarioAuth->rol == 'Cliente')
+                <li><a href="/usuario/show/{{ $usuarioAuth->usuarioID }}">Mi perfil</a></li>
+            @endif
+        </ul>
+    </div>
+@endsection
     
 @section('contenido')
 <div id="cuadro">
