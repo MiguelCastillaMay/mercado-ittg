eb6e51ea03da55734f3fb8cbf5a0c165ae35f60d
Miguel Castilla May <50216221+MiguelCastillaMay@users.noreply.github.com>
2021-06-22 20:52:58 -0500

    Cambio en el diseño del formulario de registro

--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -2,35 +2,140 @@
 
 @section('title', 'Registro')
 
+@section('navBar')
+    <div class="menuBar">
+        <h1>Mercado ITTG</h1>
+        <ul>
+            <li><a href="/categoria">Categorías</a></li>
+            <li><a href="/productos">Productos</a></li>
+            <li><form action="/search" method="get" role="search">
+                <input type="text" name="find" placeholder="Buscar productos">
+                <button type="submit">Buscar</button>
+            </form></li>
+            <li><a href="/login">Iniciar sesión</a></li>
+        </ul>
+    </div>
+@endsection
+
 <style>
+    h2 {
+        color: #1e212d;
+        font-size: 1.7em;
+        margin-bottom: 15px;
+    }
+    .cuadro {
+        padding-left: 19.920px;
+        padding-right: 19.920px;
+        background-color: aliceblue;
+        border-color: #1e212d;
+        border-style: solid;
+        border-radius: 30px;
+        border-width: 5px;
+        width: fit-content;
+        margin-right: auto;
+        margin-left: auto;
+    }
+    .nombre, .passwords{
+        display: flex;
+        justify-content: space-around;
+    }
+    #apellidoP{
+        margin-right: 30px;
+        margin-left: 30px;
+    }
+    #nombre > input, #apellidoP > input,
+    #apellidoM > input {
+        width: -webkit-fill-available;
+    }    #contra1, #contra2 {
+        width: 50%;
+    }
+    #contra1 > input, #contra2 > input {
+        width: 80%;
+    }
+    #email > input {
+        width: 40%;
+    }
+    input {
+        color: #1e212d;
+    }
+    #email {
+        display: flex;
+        flex-direction: column;
+    }
+    span {
+        color: aliceblue;
+        margin-top: 10px;
+        width: fit-content;
+        padding: 3px;
+    }
+    .registro {
+        display: block;
+        width: fit-content;
+        margin-top: 22.576px;
+        margin-bottom: 22.576px;
+        margin-right: auto;
+        margin-left: auto;
+    }
+    .error {
+        background-color: #810000;
+        border-radius: 5px;
+        border: 2px solid #810000;
+    }
+    .exito {
+        background-color: #1cc25c;
+        border-radius: 5px;
+        border: 2px solid #1cc25c;
+    }
+    #mensaje {
+        width: fit-content;
+        margin-right: auto;
+        margin-left: auto;
+        margin-top: -17.424px;
+        margin-bottom: 22.576px;
+    }
 </style>
 
 @section('contenido')
-    <div id="cuadro">
+@if (session('error'))
+    <h2 id="mensaje">{{ session('error') }}</h2>
+@endif
+    <div class="cuadro">
         <form action="/usuario/store" method="post" enctype="multipart/form-data">
             @csrf
-            <div id="form">
-                <div>
-                    <p>Nombre:</p>
-                    <p>Apellido paterno:</p>
-                    <p>Apellido materno:</p>
-                    <p>Correo:</p>
-                    <p>Imagen:</p>
-                    <p>Contraseña:</p>
-                    <p>Repita la contraseña</p>
-                </div>
-                <div id="inputs">
+            <div class="nombre">
+                <div id="nombre">
+                    <h2>Nombre</h2>
                     <input type="text" name="nombre" value="">
+                </div>
+                <div id="apellidoP">
+                    <h2>Apellido paterno</h2>
                     <input type="text" name="a_paterno" value="">
+                </div>
+                <div id="apellidoM">
+                    <h2>Apellido materno</h2>
                     <input type="text" name="a_materno" value="">
-                    <input type="text" name="correo" id="correo" value="">
-                    <span id="error"></span>
-                    <input type="file" name="imagen">
+                </div>
+            </div>
+            <div class="imagen">
+                <h2>Seleccione una imagen</h2>
+                <input type="file" name="imagen">
+            </div>
+            <div id="email">
+                <h2>Correo</h2>
+                <input type="text" name="correo" id="correo" value="">
+                <span id="mensaje"></span>
+            </div>
+            <div class="passwords">
+                <div id="contra1">
+                    <h2>Contraseña</h2>
                     <input type="password" name="password" id="password">
+                </div>
+                <div id="contra2">
+                    <h2>Confirme su contraseña</h2>
                     <input type="password" name="password2" id="password2">
                 </div>
             </div>
-            <input type="submit" value="Registrarse" id="botonInverso">
+            <input class="registro" type="submit" value="Registrarse" id="botonInverso">
         </form>
     </div>
 
@@ -45,7 +150,9 @@
                 var filter = /[\w-\.]{3,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/;
 
                 if(!filter.test(correo)) {
-                    $('#error').html('<label>Dirección de correo inválida</label>');
+                    $('#mensaje').html('<label>Dirección de correo inválida</label>');
+                    $('#mensaje').remove('exito');
+                    $('#mensaje').addClass('error');
                     $('#botonInverso').attr('disabled', 'disabled');
                 } else {
                     $.ajax({
@@ -54,12 +161,14 @@
                         data: {correo:correo, _token:_token},
                         success: function(result) {
                             if (result == 'unique') {
-                                $('#error').html('<label>Correo disponible</label>');
-                                $('#error').removeClass('error');
+                                $('#mensaje').html('<label>Correo disponible</label>');
+                                $('#mensaje').removeClass('error');
+                                $('#mensaje').addClass('exito');
                                 $('#botonInverso').attr('disabled', false);
                             } else {
-                                $('#error').html('<label>Correo no disponible</label>');
-                                $('#error').addClass('error');
+                                $('#mensaje').html('<label>Correo no disponible</label>');
+                                $('#mensaje').removeClass('exito');
+                                $('#mensaje').addClass('error');
                                 $('#botonInverso').attr('disabled', 'disabled');
                             }
                         }
